079e6fcfad0ef5f4321839779b49efd2d05f22be
bchurlinov <bchurlinov@hotmail.com>
2020-01-19 12:55:41 +0100

    prod

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/connectDB");
 const cors = require("cors");
+const path = require("path");
 
 // Initialize Express
 const app = express();
@@ -18,6 +19,17 @@ app.use(express.json({extended: false}));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+
+if(process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))

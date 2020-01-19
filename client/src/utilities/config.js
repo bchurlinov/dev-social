c5964dd2fad119e6a41da54ad22e633c13dafa15
bchurlinov <bchurlinov@hotmail.com>
2020-01-19 13:22:12 +0100

    config changed

--- client/src/utilities/config.js
+++ client/src/utilities/config.js
@@ -2,7 +2,8 @@ import _ from "lodash";
 import {Spin} from "antd";
 import React from "react";
 
-export const url = "http://localhost:5000/api";
+// export const url = "http://localhost:5000/api";
+export const url = "https://fierce-beach-01153.herokuapp.com/api";
 
 export const getUserPosition = profile => {
     if (profile) {
